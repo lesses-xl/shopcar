961758551ebf56263e9399ac037859a126c4f4d1
lesses <767733103@qq.com>
2017-10-02 20:04:43 +0800

    10.2

--- pages/shopcar/shopcar.js
+++ pages/shopcar/shopcar.js
@@ -1,3 +1,49 @@
 Page({
-	
+	data: {
+    haveThing: true,
+    shopThing: [
+      {
+        "thingName": "微软Microsoft suface pro 4",
+        "thingImg": "../../images/goods01.png",
+        "thingPrice": 12888,
+        "thingNum": 1,
+        "thingIndex": 1
+      },
+      {
+        "thingName": "微软Microsoft suface pro 4",
+        "thingImg": "../../images/goods01.png",
+        "thingPrice": 12888,
+        "thingNum": 1,
+        "thingIndex": 2
+      },
+      {
+        "thingName": "微软Microsoft suface pro 4",
+        "thingImg": "../../images/goods01.png",
+        "thingPrice": 12888,
+        "thingNum": 1,
+        "thingIndex": 3
+      },
+      {
+        "thingName": "微软Microsoft suface pro 4",
+        "thingImg": "../../images/goods01.png",
+        "thingPrice": 12888,
+        "thingNum": 1,
+        "thingIndex": 4
+      },
+      {
+        "thingName": "微软Microsoft suface pro 4",
+        "thingImg": "../../images/goods01.png",
+        "thingPrice": 12888,
+        "thingNum": 1,
+        "thingIndex": 5
+      },
+      {
+        "thingName": "微软Microsoft suface pro 4",
+        "thingImg": "../../images/goods01.png",
+        "thingPrice": 12888,
+        "thingNum": 1,
+        "thingIndex": 6
+      }
+    ]
+  }
 })
